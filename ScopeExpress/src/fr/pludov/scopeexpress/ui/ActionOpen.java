0b6008f1bf8ad03512ac441fce6d427842e5aec3
pludov <pludov@nnx.com>
2014-10-19 20:54:15 +0200

    Retenir le filtre dans le dialogue d'ouverture

--- ScopeExpress/src/fr/pludov/scopeexpress/ui/ActionOpen.java
+++ ScopeExpress/src/fr/pludov/scopeexpress/ui/ActionOpen.java
@@ -3,9 +3,14 @@ package fr.pludov.scopeexpress.ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.apache.log4j.Logger;
 
 import fr.pludov.scopeexpress.Cadrage;
 import fr.pludov.scopeexpress.focus.Image;
@@ -14,33 +19,72 @@ import fr.pludov.scopeexpress.focus.MosaicListener.ImageAddedCause;
 import fr.pludov.scopeexpress.ui.preferences.StringConfigItem;
 
 public class ActionOpen implements ActionListener {
+	private static final Logger logger = Logger.getLogger(ActionOpen.class);
+	
 	public final StringConfigItem lastOpenLocation = new StringConfigItem(ActionOpen.class, "lastOpenLocation", "");
+	public final StringConfigItem lastFilter = new StringConfigItem(ActionOpen.class, "lastOpenFilter", "");
 	
 	FocusUi focusUi;
 	JFileChooser chooser;
 	
+	List<FileNameExtensionFilter> filters;
+	
 	public ActionOpen(FocusUi focusUi) {
 		this.focusUi = focusUi;
+		this.filters = Arrays.asList(
+				new FileNameExtensionFilter("Canon raw", "cr2", "crw"),
+				new FileNameExtensionFilter("FITS", "fit", "fits")
+		);
+				
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	
+		
 		if (chooser == null) {
 			String lastOpenLocationValue = lastOpenLocation.get();
 			File currentDir = "".equals(lastOpenLocationValue) ? null : new File(lastOpenLocationValue);
 			chooser = new JFileChooser(currentDir);
-			chooser.setFileFilter(new FileNameExtensionFilter("Canon raw", "cr2", "crw"));
-			chooser.setFileFilter(new FileNameExtensionFilter("FITS", "fit", "fits"));
+			for(FileNameExtensionFilter filter : filters) {
+				chooser.addChoosableFileFilter(filter);
+			}
+			String currentExt = lastFilter.get();
+			if (currentExt != null && !"".equals(currentExt)) {
+				if (currentExt.equals("*")) {
+					chooser.setFileFilter(chooser.getAcceptAllFileFilter());
+				} else {
+					for(FileNameExtensionFilter filter : filters) {
+						if (filter.getExtensions()[0].equals(currentExt)) {
+							chooser.setFileFilter(filter);
+						}
+					}
+				}
+			}
 		}
 		
 		chooser.setMultiSelectionEnabled(true);
 		
-		if (chooser.showOpenDialog(focusUi.getFrmFocus()) != JFileChooser.APPROVE_OPTION) return;
+		try {
+			if (chooser.showOpenDialog(focusUi.getFrmFocus()) != JFileChooser.APPROVE_OPTION) return;
+		} finally {
+			// Retenir l'extension
+			FileFilter currentFilter = chooser.getFileFilter();
+			if (currentFilter instanceof FileNameExtensionFilter) {
+				FileNameExtensionFilter fi = (FileNameExtensionFilter) currentFilter;
+				lastFilter.set(fi.getExtensions()[0]);
+			} else {
+				if (currentFilter.equals(chooser.getAcceptAllFileFilter())) {
+					lastFilter.set("*");
+				} else {
+					logger.warn("unknown filter:" + currentFilter);
+				}
+			}
+			
+			// Retenir aussi le répertoire
+			File currentDir = chooser.getCurrentDirectory();
+			lastOpenLocation.set(currentDir == null ? "" : currentDir.toString());
+		}
 		
-		
-		File currentDir = chooser.getCurrentDirectory();
-		lastOpenLocation.set(currentDir == null ? "" : currentDir.toString());
 		
 		File[] files = chooser.getSelectedFiles();
 		
