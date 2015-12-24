f87c9baa20d55b1ef5bdf7407a83485ac311fb17
pludov <pludov@nnx.com>
2015-12-24 12:18:33 +0100

    Respect de l'ordre des notifs du WeakListener
    Nouveau mode de fonctionnement de WeakListenerCollection permettant
    d'assurer une distribution dans l'ordre des evenement dans le thread
    En plus, vérification que les message ne sont pas distribués à des
    listeners qui ne sont plus enregistrés

--- ScopeExpress/src/fr/pludov/scopeexpress/utils/WeakListenerCollection.java
+++ ScopeExpress/src/fr/pludov/scopeexpress/utils/WeakListenerCollection.java
@@ -22,12 +22,24 @@ import fr.pludov.scopeexpress.tasks.TaskInterruptedException;
  * => il faut que le owner ait une référence sur le listener
  */
 public class WeakListenerCollection<Interface> implements InvocationHandler, IWeakListenerCollection<Interface> {
+	public enum AsyncKind { 
+		// Listener invoqué dans le thread appellant
+		CallerThread, 
+		// Listener invoqué via SwingUtilities.invokeLatter
+		SwingQueue,
+		// Listener invoqué via SwingUtilities.invokeLatter si le thread appelant n'est pas le Thread swing
+		SwingQueueIfRequired 
+	};
+	
 	private static class ControlledObject<Interface>
 	{
-		WeakReference<WeakListenerOwner> reference;
+		final WeakReference<WeakListenerOwner> reference;
 		// Si interface garde un lien sur object, rien de tout ça ne part à la poubelle...
 		// il faudrait faire en sorte que object garde une référence sur l'interface
-		WeakReference<Interface> listener;
+		final WeakReference<Interface> listener;
+		
+		// Lorsqu'un controlled object est retiré, il est marqué "dead"
+		boolean dead;
 		
 		ControlledObject(WeakListenerOwner owner, Interface itf)
 		{
@@ -51,7 +63,7 @@ public class WeakListenerCollection<Interface> implements InvocationHandler, IWe
 	// private List<WeakReference<Interface>> listeners;
 	private List<ControlledObject<Interface>> listeners;
 	
-	private boolean async = false;
+	private final AsyncKind asyncKind;
 	
 	public WeakListenerCollection(Class<? extends Interface> clazz)
 	{
@@ -60,11 +72,15 @@ public class WeakListenerCollection<Interface> implements InvocationHandler, IWe
 	
 	public WeakListenerCollection(Class<? extends Interface> clazz, boolean async)
 	{
+		this(clazz, async ? AsyncKind.SwingQueue : AsyncKind.CallerThread);
+	}
+	
+	public WeakListenerCollection(Class<? extends Interface> clazz, AsyncKind asyncKind) {
 		listeners = new ArrayList<ControlledObject<Interface>>();
 		proxy = (Interface)Proxy.newProxyInstance(clazz.getClassLoader(), 
 				new Class[]{clazz}, 
 				this);
-		this.async = async;
+		this.asyncKind = asyncKind;
 	}
 	
 	boolean isActive(Interface target)
@@ -75,72 +91,129 @@ public class WeakListenerCollection<Interface> implements InvocationHandler, IWe
 	@Override
 	public Object invoke(Object proxy, final Method method, final Object[] args) throws Throwable 
 	{
-		List<Interface> objectToNotify = new ArrayList<Interface>();
-		// 1 - vider les références vide
-		for(Iterator<ControlledObject<Interface>> it = listeners.iterator(); it.hasNext();)
-		{
-			ControlledObject<Interface> ref = it.next();
-			Interface target = ref.get();
-			if (target == null) {
-				it.remove();
-			} else {
-				objectToNotify.add(target);
-			}
-		}
+		final List<ControlledObject<Interface>> objectToNotify = getObjectsToNotify();
 		
-		Object result = null;
-		for(final Interface target  : objectToNotify)
+		if (asyncKind == AsyncKind.CallerThread || (asyncKind == AsyncKind.SwingQueueIfRequired && SwingUtilities.isEventDispatchThread()))
 		{
-			if (!isActive(target)) {
-				continue;
-			}
-			
-			try {
-				if (!async) {
-					Object r = method.invoke(target, args);
-					if (r != null && result == null) {
-						result = r;
-					}
-				} else {
-					SwingUtilities.invokeLater(new Runnable() {
-						@Override
-						public void run() {
-							try {
-								method.invoke(target, args);
-							} catch (Throwable e) {
-								if ((e instanceof InvocationTargetException) && (e.getCause() instanceof TaskInterruptedException)) {
-									// Ignorer simplement
-								} else {
-									e.printStackTrace();
-								}
-							}
-						}
-					});
+			Object result = null;
+			for(ControlledObject<Interface> ct : objectToNotify) {
+				Object r = notifyControlledObject(ct, method, args);
+				if (r != null && result == null) {
+					result = r;
 				}
-			} catch(Throwable t) {
-				t.printStackTrace();
 			}
+			return result;
+		} else {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					for(ControlledObject<Interface> ct : objectToNotify) {
+						notifyControlledObject(ct, method, args);
+					}
+				}
+
+			});
+			return null;
+		}
+//		
+//		Object result = null;
+//		for(final Interface target  : objectToNotify)
+//		{
+//			if (!isActive(target)) {
+//				continue;
+//			}
+//			
+//			try {
+//				if (asyncKind == AsyncKind.CallerThread || (asyncKind == AsyncKind.SwingQueueIfRequired && SwingUtilities.isEventDispatchThread())) {
+//					Object r = method.invoke(target, args);
+//					if (r != null && result == null) {
+//						result = r;
+//					}
+//				} else {
+//					SwingUtilities.invokeLater(new Runnable() {
+//						@Override
+//						public void run() {
+//							try {
+//								method.invoke(target, args);
+//							} catch (Throwable e) {
+//								if ((e instanceof InvocationTargetException) && (e.getCause() instanceof TaskInterruptedException)) {
+//									// Ignorer simplement
+//								} else {
+//									e.printStackTrace();
+//								}
+//							}
+//						}
+//					});
+//				}
+//			} catch(Throwable t) {
+//				t.printStackTrace();
+//			}
+//		}
+//		
+//		return result;
+	}
+
+	private Object notifyControlledObject(ControlledObject<Interface> ct, Method method, Object[] args) {
+		Interface target = ct.get();
+		if (target == null) {
+			return null;
+		}
+		if (!isActive(target)) {
+			return null;
 		}
 		
-		return result;
+		try {
+			return method.invoke(target, args);
+		} catch (Throwable e) {
+			if ((e instanceof InvocationTargetException) && (e.getCause() instanceof TaskInterruptedException)) {
+				// Ignorer simplement
+			} else {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+	
+
+	private List<ControlledObject<Interface>> getObjectsToNotify() {
+		synchronized(listeners) {
+			List<ControlledObject<Interface>> objectToNotify = new ArrayList<>();
+			// 1 - vider les références vide
+			for(Iterator<ControlledObject<Interface>> it = listeners.iterator(); it.hasNext();)
+			{
+				ControlledObject<Interface> ref = it.next();
+				Interface target = ref.get();
+				if (target == null) {
+					it.remove();
+				} else {
+					objectToNotify.add(ref);
+				}
+			}
+			return objectToNotify;
+		}
 	}
 	
 	public void addListener(WeakListenerOwner owner, Interface i)
 	{
-		removeListener(owner);
-		listeners.add(new ControlledObject<Interface>(owner, i));
-		owner.addListener(i);
+		synchronized(listeners) {
+			removeListener(owner);
+			listeners.add(new ControlledObject<Interface>(owner, i));
+			owner.addListener(i);
+		}
 	}
 	
 	public void removeListener(WeakListenerOwner owner)
 	{
-		for(Iterator<ControlledObject<Interface>> it = listeners.iterator(); it.hasNext();)
-		{
-			ControlledObject<Interface> ref = it.next();
-			WeakListenerOwner refOwner = ref.getOwner();
-			if (refOwner == null || refOwner == owner) {
-				if (refOwner != null) refOwner.removeListener(ref.listener.get());
-				it.remove();
+		synchronized(listeners) {
+			for(Iterator<ControlledObject<Interface>> it = listeners.iterator(); it.hasNext();)
+			{
+				ControlledObject<Interface> ref = it.next();
+				WeakListenerOwner refOwner = ref.getOwner();
+				if (refOwner == null || refOwner == owner) {
+					ref.dead = true;
+					if (refOwner != null) refOwner.removeListener(ref.listener.get());
+					it.remove();
+				}
 			}
 		}
 	}
